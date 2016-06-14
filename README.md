390b7fee3197af9743fe86ebaf40984061bcea84
Rob Eisenberg <rob@bluespire.com>
2016-06-14 15:26:19 -0400

    chore(all): update build, deps, ci, d.ts gen, doc gen

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 [![npm Version](https://img.shields.io/npm/v/aurelia-validatejs.svg)](https://www.npmjs.com/package/aurelia-validatejs)
 [![ZenHub](https://raw.githubusercontent.com/ZenHubIO/support/master/zenhub-badge.png)](https://zenhub.io)
 [![Join the chat at https://gitter.im/aurelia/discuss](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/aurelia/discuss?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+[![CircleCI](https://circleci.com/gh/aurelia/validatejs.svg?style=shield)](https://circleci.com/gh/aurelia/validatejs)
 
 This is a plugin that will allow using validate.js in your Aurelia application for expressive validation. Karma/Jasmine testing is also configured.
 
--- build/paths.js
+++ build/paths.js
@@ -1,10 +1,14 @@
 var path = require('path');
 var fs = require('fs');
 
+// hide warning //
+var emitter = require('events');
+emitter.defaultMaxListeners = 20;
+
 var appRoot = 'src/';
 var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-module.exports = {
+var paths = {
   root: appRoot,
   source: appRoot + '**/*.js',
   html: appRoot + '**/*.html',
@@ -13,5 +17,23 @@ module.exports = {
   doc:'./doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/',
-  packageName: pkg.name
+  packageName: pkg.name,
+  ignore: [],
+  useTypeScriptForDTS: false,
+  importsToAdd: [],
+  sort: false
 };
+
+paths.files = [
+  'metadata-key.js',
+  'validation-rule.js',
+  'validation-rules.js',
+  'base-decorator.js',
+  'decorators.js',
+  'validator.js',
+  'index.js'
+].map(function(file){
+  return paths.root + file;
+});
+
+module.exports = paths;
